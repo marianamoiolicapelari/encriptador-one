d40711ddac4667587dc296ce90ba7883bc7a4bcb
Mariana M.C <marianamoioli@gmail.com>
2022-08-09 17:04:47 -0300

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+/*
+CRIPTOGRAFIA
+Criar a função
+Ligar a função ao botão correspondente no HTML
+Capturar o texto escrito no campo de input do HTML
+Desenvolver a lógica da criptografia
+retornar na tela o resultado do texto criptografado
+*/
+
+/*
+DESCRIPTOGRAFIA
+Criar a função
+Ligar a função ao botão correspondente no HTML
+Capturar o texto escrito no campo de input do HTML
+Desenvolver a lógica da descriptografia
+retornar na tela o resultado do texto descriptografado
+*/
+
+/*
+PROJETO
+As "chaves" de criptografia que utilizaremos são:
+A letra "e" é convertida para "enter"
+A letra "i" é convertida para "imes"
+A letra "a" é convertida para "ai"
+A letra "o" é convertida para "ober"
+A letra "u" é convertida para "ufat"
+
+Requisitos:
+- Deve funcionar apenas com letras minúsculas
+- Não devem ser utilizados letras com acentos nem caracteres especiais
+- Deve ser possível converter uma palavra para a versão criptografada e também retornar uma palavra criptografada para a versão original.
+
+Por exemplo:
+"gato" => "gaitober"
+gaitober" => "gato"
+
+A página deve ter campos para inserção do texto a ser criptografado ou descriptografado, e a pessoa usuária deve poder escolher entre as duas opções
+O resultado deve ser exibido na tela.
+Extras:
+- Um botão que copie o texto criptografado/descriptografado para a área de transferência - ou seja, que tenha a mesma funcionalidade do ctrl+C ou da opção "copiar" do menu dos aplicativos.
+
+Temos um período de tempo de duas semanas para desenvolver o projeto e vamos trabalhar com o sistema ágil de desenvolvimento, utilizando o Trello da seguinte forma:
+
+A coluna Pronto pra iniciar apresenta os cartões com os elementos ainda não desenvolvidos.
+Já na coluna Desenvolvendo ficarão os elementos que você estiver desenvolvendo no momento. Ao iniciar uma tarefa, você poderá mover o cartão que contém a tarefa para esta coluna.
+No Pausado estarão os elementos que você começou a desenvolver, mas precisou parar por algum motivo.
+Por fim, a coluna Concluído terá os elementos já concluídos.
+*/
+
