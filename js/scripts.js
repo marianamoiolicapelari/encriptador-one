c2da62b3f9de47437395fcd7e101cea715dc6f9c
Mariana M.C <marianamoioli@gmail.com>
2022-08-10 15:45:35 -0300

    finished first version

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,50 @@
-/*
-CRIPTOGRAFIA
-Criar a função
-Ligar a função ao botão correspondente no HTML
-Capturar o texto escrito no campo de input do HTML
-Desenvolver a lógica da criptografia
-retornar na tela o resultado do texto criptografado
-*/
 
-/*
-DESCRIPTOGRAFIA
-Criar a função
-Ligar a função ao botão correspondente no HTML
-Capturar o texto escrito no campo de input do HTML
-Desenvolver a lógica da descriptografia
-retornar na tela o resultado do texto descriptografado
-*/
+function encriptar() {
+    let texto = document.getElementById('input-container').value.toLowerCase()
 
-/*
-PROJETO
-As "chaves" de criptografia que utilizaremos são:
-A letra "e" é convertida para "enter"
-A letra "i" é convertida para "imes"
-A letra "a" é convertida para "ai"
-A letra "o" é convertida para "ober"
-A letra "u" é convertida para "ufat"
+    var textCifrado = texto.replace(/e/igm, "enter")
+    var textCifrado = textCifrado.replace(/o/igm, "ober")
+    var textCifrado = textCifrado.replace(/i/igm, "imes")
+    var textCifrado = textCifrado.replace(/u/igm, "ufat")
 
-Requisitos:
-- Deve funcionar apenas com letras minúsculas
-- Não devem ser utilizados letras com acentos nem caracteres especiais
-- Deve ser possível converter uma palavra para a versão criptografada e também retornar uma palavra criptografada para a versão original.
 
-Por exemplo:
-"gato" => "gaitober"
-gaitober" => "gato"
+    document.getElementById("img-section-response").style.display = "none"
+    document.querySelector(".second-paragraph").style.display = "none"
+    document.querySelector(".first-paragraph").innerHTML = textCifrado
+    document.getElementById("btn-section-response").style.display = "show"
+    document.getElementById("btn-section-response").style.display = "inherit"
+    document.getElementById("btn-section-response-refresh").style.display = "show"
+    document.getElementById("btn-section-response-refresh").style.display = "inherit"
+}
 
-A página deve ter campos para inserção do texto a ser criptografado ou descriptografado, e a pessoa usuária deve poder escolher entre as duas opções
-O resultado deve ser exibido na tela.
-Extras:
-- Um botão que copie o texto criptografado/descriptografado para a área de transferência - ou seja, que tenha a mesma funcionalidade do ctrl+C ou da opção "copiar" do menu dos aplicativos.
 
-Temos um período de tempo de duas semanas para desenvolver o projeto e vamos trabalhar com o sistema ágil de desenvolvimento, utilizando o Trello da seguinte forma:
+function desencriptar() {
+    let texto = document.getElementById('input-container').value.toLowerCase()
 
-A coluna Pronto pra iniciar apresenta os cartões com os elementos ainda não desenvolvidos.
-Já na coluna Desenvolvendo ficarão os elementos que você estiver desenvolvendo no momento. Ao iniciar uma tarefa, você poderá mover o cartão que contém a tarefa para esta coluna.
-No Pausado estarão os elementos que você começou a desenvolver, mas precisou parar por algum motivo.
-Por fim, a coluna Concluído terá os elementos já concluídos.
-*/
+    var textCifrado = texto.replace(/enter/igm, "e")
+    var textCifrado = textCifrado.replace(/ober/igm, "o")
+    var textCifrado = textCifrado.replace(/imes/igm, "i")
+    var textCifrado = textCifrado.replace(/ai/igm, "a")
+    var textCifrado = textCifrado.replace(/ufat/igm, "u")
 
+
+    document.getElementById("img-section-response").style.display = "none"
+    document.querySelector(".second-paragraph").style.display = "none"
+    document.querySelector(".first-paragraph").innerHTML = textCifrado
+}
+
+function copyToClickBoard() {
+    let texto = document.getElementById('input-container')
+    let content = document.querySelector('.first-paragraph').innerHTML;
+
+    navigator.clipboard.writeText(content)
+    alert("Copiado!")
+
+    texto.value = ""
+}
+
+let buttonRefresh = document.querySelector("#btn-section-response-refresh");
+buttonRefresh.addEventListener("click", function () {
+
+    location.reload();
+});
